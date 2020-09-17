0af480a7c43507463822961d2118140db964453a
Luc Claustres <luc.claustres@orange.fr>
2020-09-17 11:54:29 +0200

    Foixed https://github.com/kalisio/kli/issues/2

--- index.js
+++ index.js
@@ -67,10 +67,9 @@ async function run (workspace, branch) {
       if (program.install) {
         await runCommand('yarn install')
       }
-      if (!options.application && (program.link || program.unlink)) {
-        console.log(program.link ? `Linking global module ${module}` : `Unlinking global module ${module}`)
-        if (program.link) await runCommand('yarn link')
-        else await runCommand('yarn unlink')
+      if (!options.application && program.link) {
+        console.log(`Linking global module ${module}`)
+        await runCommand('yarn link')
       }
       if (options.application) {
         shell.cd('api')
@@ -121,6 +120,32 @@ async function run (workspace, branch) {
       }
       shell.cd(cwd)
     }
+  }
+  
+  for (let i = 0; i < modules.length; i++) {
+    const module = modules[i]
+    const options = workspace[module]
+    if (program.modules && !program.modules.includes(module)) {
+      continue
+    }
+    if (options.branches && !options.branches.includes(branch)) {
+      continue
+    }
+    console.log(`Finalizing module ${module}`)
+    const cwd = process.cwd()
+    // Working path for module can be relative to CWD when managing code for different organizations (eg kalisio/weacast)
+    // CWD is the root path for the "main" organization usually owing the project
+    shell.cd(options.path ? path.join(cwd, options.path, `${module}`) : path.join(cwd, `${module}`))
+    try {
+      // Now we have unlinked removed global links
+      if (!options.application && program.unlink) {
+        console.log(`Unlinking global module ${module}`)
+        await runCommand('yarn unlink')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+    shell.cd(cwd)
   }
 }
 
