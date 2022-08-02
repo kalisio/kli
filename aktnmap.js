6defdfa318caa6f3180ff0bf86b025b14144f7dd
Luc Claustres <luc.claustres@orange.fr>
2022-08-02 13:18:45 +0200

    chore: added aktnmap workspaces

---
+++ aktnmap.js
@@ -0,0 +1,34 @@
+module.exports = {
+  'feathers-distributed': {
+    dependencies: [],
+    branch: 'buzzard'
+  },
+  'weacast-core': {
+    organization: 'weacast',
+    dependencies: [],
+    path: '../weacast',
+    branch: 'master'
+  },
+  'weacast-leaflet': {
+    organization: 'weacast',
+    dependencies: [
+      'weacast-core'
+    ],
+    path: '../weacast',
+    branch: 'master'
+  },
+  kdk: {
+    dependencies: [
+      'weacast-core', 
+      'weacast-leaflet'
+    ]
+  },
+  aktnmap: {
+    application: true,
+    dependencies: [
+      '@kalisio/feathers-distributed',
+      'weacast-core', 
+      'weacast-leaflet',
+      '@kalisio/kdk']
+  }
+}
